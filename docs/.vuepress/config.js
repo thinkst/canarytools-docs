5b96644b2766fda0901fe98ca3ecaa5d9153545e
Nicholas Rohrbeck <nick@thinkst.com>
2020-03-31 22:55:47 +0200

    Add log and intial PWA bits

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,29 @@
 module.exports = {
-  title: 'Canarytools Docs',
+  title: 'Canary API Docs',
   description: 'Documentation for the Canary Console.',
   markdown: {
     lineNumbers: false,
   },
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#38D47F' }],
+    // ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    // ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    // ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    // ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    // ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    // ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
   plugins: [
     'tabs',
-    ['vuepress-plugin-code-copy', {
-      align: 'top',
-      color: '#38D47F'
-    }],
+    [
+      'vuepress-plugin-code-copy',
+      {
+        align: 'top',
+        color: '#38D47F'
+      }
+    ],
     [
       'vuepress-plugin-container',
       {
@@ -22,6 +36,13 @@ module.exports = {
         after: '</div>',
       },
     ],
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: true
+      }
+    ],
   ],
   themeConfig: {
     docsRepo: 'thinkst/canarytools-docs',
@@ -30,6 +51,7 @@ module.exports = {
     editLinks: true,
     editLinkText: 'Help us improve this page!',
     lastUpdated: 'Last Updated', // string | boolean
+    logo: '/logo.png',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/getting-started' },
